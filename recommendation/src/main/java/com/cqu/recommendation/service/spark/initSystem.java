ea153bce16d901508c544512fd77013796211ea0
Anders <13895637677@126.com>
2019-12-11 05:23:08 +0800

    init

--- recommendation/src/main/java/com/cqu/recommendation/service/spark/initSystem.java
+++ recommendation/src/main/java/com/cqu/recommendation/service/spark/initSystem.java
@@ -11,6 +11,7 @@ import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.launcher.SparkLauncher;
 import org.apache.spark.rdd.RDD;
+import org.apache.spark.sql.SparkSession;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -52,16 +53,21 @@ public class initSystem {
         }
     }
     public static void main(String[] args) {
-        //1、测试HDFS文件访问
-        Configuration conf1 = new Configuration();
-        String txtFilePath = "hdfs://192.168.1.112:9000/users/part-r-00000";
-        List<String> lines = getStringsByTXT(txtFilePath, conf1, "");
+//        //1、测试HDFS文件访问
+//        Configuration conf1 = new Configuration();
+//        String txtFilePath = "hdfs://192.168.1.112:9000/users/part-r-00000";
+//        List<String> lines = getStringsByTXT(txtFilePath, conf1, "");
 
         //2、测试Spark访问
         SparkConf conf = new SparkConf().setAppName("Spark Java API 学习")
-                .setMaster("spark://192.168.1.112:7077")
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> users = sc.textFile("hdfs://192.168.1.112:9000/users");
+                .setMaster("spark://192.168.217.132:7077");
+        SparkSession sparkSession = SparkSession.builder().config(conf).getOrCreate();
+
+        RDD<String> stringRDD = sparkSession.sparkContext().textFile("S:\\develop\\WorkSpace\\bigData\\recommendation\\src\\main\\resources\\movies.csv",1);
+
+
+
         System.out.println(users.first());
+        sc.close();
     }
 }
